ed5e174026e3749cdb4005cf8d934b4905e5cc0a
Yurij <groonel@localhost.localdomain>
2019-06-03 19:24:59 +0300

    change all

--- api/config/bundles.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
-];
---
+++ projects/api/public/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: groonel
+ * Date: 03.06.19
+ * Time: 18:58
+ */
+echo phpinfo();
